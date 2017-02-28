b1f41be32bb8e04aeea9cb4f4653d5cd1a8ea437
Andrew Donnell <asdonnell@gmail.com>
2017-02-28 17:01:27 -0500

    Create new user badge selection UI

---
+++ SweetCorn/SweetCorn/BadgeSelectorViewController.swift
@@ -0,0 +1,27 @@
+//
+//  BadgeSelectorViewController.swift
+//  SweetCorn
+//
+//  Created by Andrew Donnell on 2/28/17.
+//  Copyright © 2017 SweetCorn Inc. All rights reserved.
+//
+
+import UIKit
+
+class BadgeSelectorViewController: UIViewController {
+
+    @IBOutlet weak var continueBtn: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        continueBtn.layer.cornerRadius = 8
+        continueBtn.layer.borderWidth = 1
+        continueBtn.layer.borderColor = UIColor(hue: 140, saturation: 36, brightness: 87, alpha: 1).cgColor
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
--- SweetCorn/SweetCorn/LoginViewController.swift
+++ SweetCorn/SweetCorn/LoginViewController.swift
@@ -54,7 +54,6 @@ class LoginViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
